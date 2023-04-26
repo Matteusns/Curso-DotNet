416a713f3fc634854e7e58dbcaf2701740950856
Mateus Nascimento <mateusnsousa77@gmail.com>
2023-04-26 11:08:27 -0300

    Fist commit

---
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody]Filme filme)
+    {
+        
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);  
+    }
+
+}
---
+++ FilmesApi/FilmesApi/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O titulo do filme é Obrigatório")]
+    [StringLength(100)]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O Genero do filme é Obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do Genero não pode passar de 50")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A Duração do filme é Obrigatório")]
+    [Range(70, 600, ErrorMessage = "A duração só pode ser entre 70 - 600 min")]
+    public int Duracao { get; set; }
+}
